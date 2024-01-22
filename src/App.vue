8c84e14978c2e69fdc205c96eeb507456761531b
zhangzejiang <rushsky158@163.com>
2024-01-22 15:47:20 +0800

    实时显示时间

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ ElNotification({
   message: h(
     "a",
     { style: "color: teal", target: "_blank", href: "https://github.com/un-pany/v3-admin-vite" },
-    "小项目获取 star 不易，如果你喜欢这个项目的话，欢迎点击这里支持一个 star ！这是作者持续维护的唯一动力（小声：毕竟是免费的）"
+    "从这个项目开始学习前端"
   ),
   duration: 0,
   position: "bottom-right"
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,11 +2,35 @@
 import { useUserStore } from "@/store/modules/user"
 import Admin from "./components/Admin.vue"
 import Editor from "./components/Editor.vue"
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue';
 
 const userStore = useUserStore()
 const isAdmin = userStore.roles.includes("admin")
+
+
+// 创建一个响应式的当前时间变量
+const currentTime = ref<Date>(new Date());
+// 定义一个每秒更新时间的定时器
+let timer: ReturnType<typeof setInterval> | null = null;
+onMounted(() => {
+// 每秒钟更新一次时间
+  timer = setInterval(() => {
+    currentTime.value = new Date();
+  }, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(timer!);
+});
+
+
 </script>
 
 <template>
+  <div>
+    <h1>
+      实时时间：{{ currentTime }}
+    </h1>
+  </div>
   <component :is="isAdmin ? Admin : Editor" />
 </template>
